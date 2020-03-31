44df678998c2dd2ee9d8a1ef9d4479754ec73b7e
japerry911 <jack.e.perry@outlook.com>
2020-03-30 19:25:04 -0600

    Added in styling to the FlatList of Posts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import { Context as BlogContext } from '../context/BlogContext';
+import { FontAwesome } from '@expo/vector-icons';
 
 const IndexScreen = () => {
     const blogContext = useContext(BlogContext);
@@ -9,7 +10,6 @@ const IndexScreen = () => {
 
     return (
         <View>
-            <Text>Index Screen!</Text>
             <Button
                 title='Add Blog Post'
                 onPress={addBlogPost}
@@ -19,7 +19,13 @@ const IndexScreen = () => {
                 keyExtractor={post => post.title}
                 renderItem={({ item }) => {
                 return (
-                    <Text>{item.title}</Text>
+                    <View style={styles.rowStyle}>
+                        <Text style={styles.titleStyle}>{item.title}</Text>
+                        <FontAwesome 
+                            name='trash' 
+                            style={styles.iconStyle}
+                        />
+                    </View>
                 );
                 }}
             />
@@ -27,6 +33,21 @@ const IndexScreen = () => {
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    rowStyle: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        borderBottomWidth: 1,
+        borderColor: 'gray',
+        paddingHorizontal: 10
+    },
+    titleStyle: {
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 24
+    }
+});
 
 export default IndexScreen;
