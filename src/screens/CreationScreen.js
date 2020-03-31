f40c84bb6c521cd7d7c102fdee623b1f6d0b0af0
japerry911 <jack.e.perry@outlook.com>
2020-03-30 22:42:20 -0600

    boilerplate CreationScreen.js setup/navigation setup

---
+++ src/screens/CreationScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CreationScreen = () => {
+    return (
+        <View>
+            <Text>Creation Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreationScreen;
